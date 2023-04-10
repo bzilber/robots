d884b50f29455fc7542118f32499a151097609f0
Ben <bzilber@users.noreply.github.com>
2023-04-09 22:50:45 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-class App extends React.Component() {
+class App extends React.Component{
   render() {
     return (
       <div className="App">
@@ -16,7 +17,7 @@ class App extends React.Component() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React!!!!
           </a>
         </header>
       </div>
