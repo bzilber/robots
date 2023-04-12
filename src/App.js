0d9e6b731f72377a2d585999fb42c56e96c40374
Ben <bzilber@users.noreply.github.com>
2023-04-12 12:34:35 -0600

    searchBox added.

--- src/App.js
+++ src/App.js
@@ -10,14 +10,22 @@ class App extends Component {
         this.state = {
             robots: robots,
             searchField: '',
-        }
+        }        
     }
+    
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value})
+    }
+
     render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
         return (
-            <div className='tc'>
-                <h1>Robots</h1>
-                <SearchBox/>
-                <CardList robots={robots}/>
+            <div className='tc bg-navy'>
+                <h1 className = 'bg-green yellow'>Robots</h1>
+                <SearchBox searchChange = {this.onSearchChange} /> {/*use 'this.' cause we're using App object from its class and not a func.*/}
+                <CardList robots={filteredRobots}/>
             </div>
         );
     }
