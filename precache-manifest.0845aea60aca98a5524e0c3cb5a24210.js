65bdd5df95e38d96f6cd989540cdb0f268d0222e
Ben <bzilber@users.noreply.github.com>
2023-04-13 21:51:09 -0600

    Updates

---
+++ precache-manifest.0845aea60aca98a5524e0c3cb5a24210.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c57b3f62802b125fe758",
+    "url": "/robots/static/css/main.3e194a40.chunk.css"
+  },
+  {
+    "revision": "c57b3f62802b125fe758",
+    "url": "/robots/static/js/main.c57b3f62.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robots/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "23b615e1b075a6c9b9ee",
+    "url": "/robots/static/css/2.ab50027c.chunk.css"
+  },
+  {
+    "revision": "23b615e1b075a6c9b9ee",
+    "url": "/robots/static/js/2.23b615e1.chunk.js"
+  },
+  {
+    "revision": "fc20b50a89803174e336",
+    "url": "/robots/static/js/runtime~main.fc20b50a.js"
+  },
+  {
+    "revision": "cbbd7cb7c68f9edcdf8854a789936742",
+    "url": "/robots/static/media/badrobot3d.cbbd7cb7.woff"
+  },
+  {
+    "revision": "fbf86826c7f4f254c9fff71e451ab266",
+    "url": "/robots/index.html"
+  }
+];
